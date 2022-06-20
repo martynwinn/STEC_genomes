7b7ba958b0f6c36a174181e4549557394621e2a5
Martyn Winn <martyn.winn@stfc.ac.uk>
2022-06-20 22:58:54 +0100

    Improve handling of read data and target gene options.

--- scripts/map_reads_to_genes.py
+++ scripts/map_reads_to_genes.py
@@ -6,51 +6,71 @@ import os, sys, glob
 import numpy as np
 import pandas as pd
 
+# isolates
+# options: deer, mince, cheese, clinical
+isolate_reads = 'clinical'
+
 # genes to search for
-#ref_indices = 'stx_genes'
-ref_indices = 'virulence_all'
+# options: 'stx_genes', 'virulence_all'
+ref_indices = 'stx_genes'
 os.environ['BOWTIE2_INDEXES'] = '/work4/scd/scarf228/genome_STEC/bowtie_indices'
 
 # best to create pandas dataframe in one go after loop
-#isolate_cols = ['name','serotype','stx_Anne','stx2a','stx2c','stx1a','stx1c','stx1d','stx2d','stx2b','stx2e','stx2f','stx2g']
-isolate_cols = ['name','serotype','simpPatho','tot_reads']
-#isolate_cols = ['name','seqtype','stx_Tim','tot_reads']
-#isolate_cols = ['name','serotype','stx_Anne','tot_reads']
-#isolate_cols = ['name','Mairi_row','serotype','tot_reads']
-for igene in range(957):
-    label = 'v'+str(igene+1)
-    isolate_cols.append(label)
+if isolate_reads == 'deer':
+    # These cols for deer, with annotation from Mairi these
+    isolate_cols = ['name','Mairi_row','serotype','tot_reads']
+    metadata_file = 'strain_metadata_reads1.tab'
+    reads_dir = 'deer_moredun/reads_batch_1/'
+elif isolate_reads == 'mince':
+    # These cols for mince, with stx annotation from Anne Holmes
+    isolate_cols = ['name','serotype','stx_Anne','tot_reads']
+    metadata_file = 'strain_metadata_mince.tab'
+    reads_dir = 'mince/'
+elif isolate_reads == 'cheese': 
+    # These cols for cheese, with stx annotation from Tim
+    isolate_cols = ['name','seqtype','stx_Tim','tot_reads']
+    metadata_file = 'strain_metadata_cheese.tab'
+    reads_dir = 'errington/'
+elif isolate_reads == 'clinical':
+    isolate_cols = ['name','serotype','simpPatho','tot_reads']
+    metadata_file = 'strain_metadata_clinical2.tab'
+    reads_dir = 'clinical_claire/batch_2/'
+
+if ref_indices == 'stx_genes':
+    isolate_cols.extend(['stx2a','stx2c','stx1a','stx1c','stx1d','stx2d','stx2b','stx2e','stx2f','stx2g'])
+elif ref_indices == 'virulence_all':
+    for igene in range(957):
+        label = 'v'+str(igene+1)
+        isolate_cols.append(label)
 ncols = len(isolate_cols)
 
 # read in strain metadata
 metadata = {}
-with open('strain_metadata_clinical2.tab','r') as f:
+with open(metadata_file,'r') as f:
     line = f.readline()
     while line:
         metadata[line.split()[0]] = [line.split()[1],line.split()[2]]
         line = f.readline()
 
-# STEC samples to search through
-#reads_dir = 'deer_moredun/reads_batch_3/'
-#reads_dir = 'mince/'
-#reads_dir = 'errington/'
-reads_dir = 'clinical_claire/batch_2/'
-
 data = []
 for isolate_file in glob.glob(reads_dir + '*_1.fastq'):
-    #MINCE file_stem = os.path.basename(isolate_file)[:-18]
-    file_stem = os.path.basename(isolate_file)[:-8]
     #DEER file_stem = os.path.basename(isolate_file)[:-16]
     #isolate = file_stem[:file_stem.index('_S')]
+    #MINCE file_stem = os.path.basename(isolate_file)[:-18]
+    #file_stem = os.path.basename(isolate_file)[:-18]
+    #CHEESE file_stem = os.path.basename(isolate_file)[:-8]
+    #file_stem = os.path.basename(isolate_file)[:-8]
+    #CLINICAL file_stem = os.path.basename(isolate_file)[:-8]
+    file_stem = os.path.basename(isolate_file)[:-8]
     isolate = file_stem
     print('Mapping ',isolate)
 
     pair_1 = reads_dir + file_stem + '_1.fastq'
     pair_2 = reads_dir + file_stem + '_2.fastq'
-    out_sam = reads_dir + file_stem + '_bowtie_vall.sam'
-    out_bam_sorted = reads_dir + file_stem + '_bowtie_vall_sorted.bam'
-    out_log = reads_dir + file_stem + '_bowtie_vall.log'
-    samtools_log = reads_dir + file_stem + '_samtools_vall.log'
+    out_sam = reads_dir + file_stem + '_bowtie_stx.sam'
+    out_bam_sorted = reads_dir + file_stem + '_bowtie_stx_sorted.bam'
+    out_log = reads_dir + file_stem + '_bowtie_stx.log'
+    samtools_log = reads_dir + file_stem + '_samtools_stx.log'
 
     isolate_data = [isolate,metadata[isolate][0],metadata[isolate][1]]
 
@@ -89,4 +109,4 @@ for isolate_file in glob.glob(reads_dir + '*_1.fastq'):
 print(data)
 df = pd.DataFrame(data,columns=isolate_cols)
 
-df.to_csv('mapped_reads_virulence_all_clinical2.csv')
+df.to_csv('mapped_reads_stxgenes_clinical2.csv')
